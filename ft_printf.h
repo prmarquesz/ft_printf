f5080cda3ad9f99eb7ff00c92d8a8d2e12fdac6a
Paulo Marques <paulormarques21@gmail.com>
2021-07-16 16:01:10 -0300

    Header: function structuring
    - definition of constants
    - construction of the specification structure

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 00:41:03 by proberto          #+#    #+#             */
-/*   Updated: 2021/07/03 03:04:30 by proberto         ###   ########.fr       */
+/*   Updated: 2021/07/16 18:09:00 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,63 @@
 
 # include "libft/libft.h"
 # include <stdarg.h>
+
+# define ON		1
+# define OFF	0
+# define CHR	-1
+# define STRING	-2
+# define INTEG	-3
+# define PTR	-4
+
+typedef struct s_flag
+{
+	int		status;
+	char	token;
+}			t_flag;
+
+typedef struct s_width
+{
+	unsigned int	value;
+	char			fill;	
+}					t_width;
+
+typedef struct s_precision
+{
+	unsigned int	value;
+	int				status;
+	char			fill;
+}					t_precision;
+
+typedef union s_value_type
+{
+	char			*svalue;
+	unsigned int	uvalue;
+	uintptr_t		pvalue;
+	int				value;
+}					t_value_type;
+
+typedef union u_length
+{
+	unsigned int	digits;
+	size_t			len;
+}					t_length;
+
+typedef struct s_data
+{
+	char			token;
+	int				type;
+	unsigned int	base;
+	t_value_type	value;
+	t_length		length;
+}					t_data;
+
+typedef struct s_spec
+{
+	t_flag		flag;
+	t_width		width;
+	t_precision	precision;
+	t_data		data;
+}				t_spec;
 
 int	ft_printf(const char *str, ...);
 
