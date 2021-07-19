0ddfe83812b0f1ec8fdff25c34ac711fe304b4c0
Paulo Marques <paulormarques21@gmail.com>
2021-07-19 03:33:02 -0300

    role restructuring

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 00:37:15 by proberto          #+#    #+#             */
-/*   Updated: 2021/07/16 18:22:35 by proberto         ###   ########.fr       */
+/*   Updated: 2021/07/19 02:55:51 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	get_spec(char **str, va_list *arg, t_spec *spec, size_t *count);
+static char	*get_spec(char *str, va_list *arg, t_spec *spec, size_t *count);
 
 int	ft_printf(const char *str, ...)
 {
@@ -25,38 +25,41 @@ int	ft_printf(const char *str, ...)
 	while (*str)
 	{
 		if (*str != '%')
-			ft_putchar_fd(*str++, 1);
+			count += ft_putchar_fd(*str++, 1);
 		else
-			if(!get_spec((char **)&str, &arg, &spec, &count))
+		{
+			str = get_spec((char *)++str, &arg, &spec, &count);
+			if (!str)
 				break ;
+		}
 	}
+	if (spec.data.type == STRING)
+		free(spec.data.value.svalue);
 	va_end(arg);
 	return (count);
 }
 
-static int		get_spec(char **str, va_list *arg, t_spec *spec, size_t *count)
+static char	*get_spec(char *str, va_list *arg, t_spec *spec, size_t *count)
 {
-	get_flag(str, spec);
-	get_width(str, spec);
-	get_precision(str, spec);
-	if (!get_data_type(str, arg, spec))
-		return (0);
+	str = get_flag(str, spec);
+	str = get_width(str, arg, spec);
+	str = get_precision(str, arg, spec);
+	str = get_data_type(str, arg, spec);
+	if (!str)
+		return (str);
 	ft_formatting(spec);
 	if (spec->data.type == CHR)
-		ft_write_char(spec);
+		ft_write_char(spec, count);
 	else if (spec->data.type == STRING)
-		ft_write_string(spec);
-	else if (spec->data.type == INTEG && spec->data.base == 10)
-		ft_write_int10(spec);
-	else if (spec->data.type == INTEG && spec->data.base == 16)
-		ft_write_int16(spec);
-	else if (spec->data.type == PTR)
-		ft_write_ptr(spec);
-	if (spec->data.type >= 0)
+		ft_write_str(spec, count);
+	else if (spec->data.type == INTEG || spec->data.type == PTR)
+		ft_write_int(spec, count);
+	/*if (spec->data.type >= 0)
 		return (0);
 	else
 	{
 		*str++;
 		return (1);
-	}
+	}*/
+	return (str);
 }
